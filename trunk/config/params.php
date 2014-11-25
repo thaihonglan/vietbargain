4adaf4fa1b218e3318e6e9f4ed5b416aa373be69
joephankm@gmail.com <joephankm@gmail.com@2c5846bf-b5bc-a74a-191c-b3306825488e>
2014-11-25 18:39:15 +0000

    midle commit

--- trunk/config/params.php
+++ trunk/config/params.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+	'adminEmail' => 'admin@example.com',
+
+	// all language available in this site
+	'languages' => [
+		'en-US' => 'english',
+		'vi-VN' => 'vietnamese',
+	],
 ];
---
+++ trunk/modules/admin/AppAsset.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\modules\admin;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+	public $basePath = '@webroot/admin';
+
+	public $baseUrl = '@web/admin';
+
+	public $css = [
+		'css/site.css',
+		'css/timeline.css',
+		'css/sb-admin-2.css',
+	];
+
+	public $js = [
+		'js/sb-admin-2.js',
+	];
+
+	public $depends = [
+		'yii\web\JqueryAsset',
+		'bootstrap',
+		'metisMenu',// blank
+		'fontAwesome',//blank
+	];
+}
