9a2cd8c815531f8c4e8b96817971290e4c55e197
joephankm@gmail.com <joephankm@gmail.com@2c5846bf-b5bc-a74a-191c-b3306825488e>
2014-11-27 17:55:22 +0000

    middle commit

--- trunk/modules/admin/views/post-type-manager/_form.php
+++ trunk/modules/admin/views/post-type-manager/_form.php
@@ -10,19 +10,33 @@ use yii\widgets\ActiveForm;
 
 <div class="post-type-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+	<?php $form = ActiveForm::begin(); ?>
 
 <?php
-    foreach (Yii::$app->params['languages'] as $code => $name):
-        list($shortCode) = explode('-', $code);
+	foreach (Yii::$app->params['languages'] as $code => $name):
+		list($shortCode) = explode('-', $code);
 ?>
-    <?= $form->field($model, 'name_' . $shortCode)->textInput() ?>
+	<?= $form->field($model, 'name_' . $shortCode)->textInput() ?>
 <?php endforeach; ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
+	<div class="form-group">
+		<?= Html::a(Html::button(Yii::t('app', 'Back'), ['class' => 'btn btn-default']), ['index']) ?>
+		<?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+	</div>
 
-    <?php ActiveForm::end(); ?>
+	<?php ActiveForm::end(); ?>
 
+	<div>
+		<?php
+			if (!$model->isNewRecord) {
+				echo Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+					'class' => 'btn btn-danger',
+					'data' => [
+						'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+						'method' => 'post',
+					],
+				]);
+			}
+		?>
+	</div>
 </div>
---
+++ trunk/modules/admin/views/user-manager/index.php
@@ -0,0 +1,68 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\admin\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Users');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<p>
+	<?= Html::a(Yii::t('app', 'Create {modelClass}', [
+    'modelClass' => 'User',
+]), ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+
+<div class="col-lg-12 user-index">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			User list
+		</div>
+		<!-- /.panel-heading -->
+
+		<div class="panel-body">
+			<div class="table-responsive">
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+	<?= GridView::widget([
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+        'columns' => [
+            'id',
+            'email:email',
+            'password',
+            'facebook_login_id',
+            'first_name',
+            // 'last_name',
+            // 'identifier',
+            // 'city_id',
+            // 'address',
+            // 'age',
+            // 'contact_number',
+            // 'avatar',
+            // 'is_unlimited_user',
+            // 'create_datetime',
+            // 'status',
+
+			[
+				'class' => 'yii\grid\ActionColumn',
+				'template' => '{update}',
+				'buttons' => [
+					'update' => function($url, $model) {
+						return '<a href="' . $url . '"><button type="button" class="btn btn-primary">Update</button></a>';
+					},
+				],
+			],
+		],
+	]); ?>
+			</div>
+			<!-- /.table-responsive -->
+		</div>
+	</div>
+</div>
+<!-- /.col-lg-12 -->
