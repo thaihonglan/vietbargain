753803591ebdf8d5fd6f1dead35a73bccfacba2f
joephankm@gmail.com <joephankm@gmail.com@2c5846bf-b5bc-a74a-191c-b3306825488e>
2014-12-07 17:45:29 +0000

    middle commit

--- trunk/modules/home/views/auth/login.php
+++ trunk/modules/home/views/auth/login.php
@@ -6,26 +6,26 @@ use yii\helpers\Html;
 ?>
 
 <div class="post">
-    <?php $form = ActiveForm::begin(['options' => ['class' => 'form newtopic', 'id' => 'login-form']]); ?>
-        <div class="postinfotop">
-            <h2><?php echo Yii::t('admin', 'Login'); ?></h2>
-        </div>
+	<?php $form = ActiveForm::begin(['options' => ['class' => 'form newtopic', 'id' => 'login-form']]); ?>
+		<div class="postinfotop">
+			<h2><?php echo Yii::t('admin', 'Login'); ?></h2>
+		</div>
 
-        <div class="login-panel panel panel-default">
-            <div class="panel-body">
-                <fieldset>
-                    <?= $form->field($model, 'email')->textInput(['placeholder' => 'Email']) ?>
+		<div class="login-panel panel panel-default">
+			<div class="panel-body">
+				<fieldset>
+					<?= $form->field($model, 'email')->textInput(['placeholder' => 'Email']) ?>
 
-                    <?= $form->field($model, 'password')->passwordInput(['placeholder' => Yii::t('admin', 'Password')]) ?>
-                    <!-- Change this to a button or input when using this as a form -->
-                    <a href="javascript:$('form#login-form').submit();" class="btn btn-lg btn-success btn-block">
-                        <?php echo Yii::t('admin', 'Login'); ?></a>
-                </fieldset>
+					<?= $form->field($model, 'password')->passwordInput(['placeholder' => Yii::t('admin', 'Password')]) ?>
+					<!-- Change this to a button or input when using this as a form -->
+					<a href="javascript:void(0)" class="btn btn-lg btn-success btn-block" onClick="$('form#login-form').submit();">
+						<?php echo Yii::t('admin', 'Login'); ?></a>
+				</fieldset>
 
-                <?= Html::a(Yii::t('admin', 'Forget password?'), ['recover-password']) ?>
-            </div>
-        </div>
+				<?= Html::a(Yii::t('admin', 'Forget password?'), ['recover-password']) ?>
+			</div>
+		</div>
 
-    <?php ActiveForm::end(); ?>
+	<?php ActiveForm::end(); ?>
 
 </div>
