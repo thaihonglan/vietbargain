92bad4d4c705729302ea2b63f3f54918f91b3108
thaihonglan@gmail.com <thaihonglan@gmail.com@2c5846bf-b5bc-a74a-191c-b3306825488e>
2014-12-08 18:26:01 +0000

    Tách riêng xét "!$user" và "!$validatePassword" trong LoginForm

--- trunk/mail/home/user-register-confirm.php
+++ trunk/mail/home/user-register-confirm.php
@@ -1,6 +1,8 @@
 <?php
 use yii\helpers\Html;
-
+use Yii;
+use yii\base\Model;
+use app\models\User;
 /* @var $this \yii\web\View view component instance */
 /* @var $message \yii\mail\MessageInterface the message being composed */
 /* @var $content string main view render result */
@@ -8,7 +10,7 @@ use yii\helpers\Html;
 
 Chúc mừng bạn đã đăng ký thành công !
 
-<p><? echo $User->first_name . ' ' . $User->last_name .','; ?> bạn hãy click vào link bên dưới để kích hoạt tài khoản :</p>
+<p><? echo User::first_name . ' ' . User::last_name .','; ?> bạn hãy click vào link bên dưới để kích hoạt tài khoản :</p>
 
 <?= $confirmLink ?>
 
