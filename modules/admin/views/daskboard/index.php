c2cbac3a5793f06868d963d002a933130142ec26
thaihonglan <thaihonglan@gmail.com>
2014-12-28 07:57:17 +0000

    dashboard

--- modules/admin/views/daskboard/index.php
+++ modules/admin/views/daskboard/index.php
@@ -1,9 +1,85 @@
 <?php
 /* @var $this yii\web\View */
-?>
-<h1>daskboard/index</h1>
+require('/../vendor/gapi13/gapi.class.php');
+use dosamigos\chartjs\Chart;
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+?>
+
+<?php
+define('ga_email','heruka.ga@gmail.com');
+define('ga_password','chanhatho');
+define('ga_profile_id','94005637');
+
+// require 'gapi.class.php';
+
+$ga = new gapi(ga_email,ga_password);
+
+$ga->requestReportData(ga_profile_id,array('browser','browserVersion'),array('pageviews','visits'));
+?>
+<table>
+<tr>
+  <th>Browser &amp; Version</th>
+  <th>Pageviews</th>
+  <th>Visits</th>
+</tr>
+<?php
+foreach($ga->getResults() as $result):
+?>
+<tr>
+  <td><?php echo $result ?></td>
+  <td><?php echo $result->getPageviews() ?></td>
+  <td><?php echo $result->getVisits() ?></td>
+</tr>
+<?php
+endforeach
+?>
+</table>
+
+<table>
+<tr>
+  <th>Total Results</th>
+  <td><?php echo $ga->getTotalResults() ?></td>
+</tr>
+<tr>
+  <th>Total Pageviews</th>
+  <td><?php echo $ga->getPageviews() ?>
+</tr>
+<tr>
+  <th>Total Visits</th>
+  <td><?php echo $ga->getVisits() ?></td>
+</tr>
+<tr>
+  <th>Results Updated</th>
+  <td><?php echo $ga->getUpdated() ?></td>
+</tr>
+</table>
+
+
+
+<?= Chart::widget([
+    'type' => 'Line',
+    'options' => [
+        'height' => 400,
+        'width' => 400
+    ],
+    'data' => [
+        'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+        'datasets' => [
+            [
+                'fillColor' => "rgba(220,220,220,0.5)",
+                'strokeColor' => "rgba(220,220,220,1)",
+                'pointColor' => "rgba(220,220,220,1)",
+                'pointStrokeColor' => "#fff",
+                'data' => [65, 59, 90, 81, 56, 55, 40]
+            ],
+            [
+                'fillColor' => "rgba(151,187,205,0.5)",
+                'strokeColor' => "rgba(151,187,205,1)",
+                'pointColor' => "rgba(151,187,205,1)",
+                'pointStrokeColor' => "#fff",
+                'data' => [28, 48, 40, 19, 96, 27, 100]
+            ]
+        ]
+    ]
+]);
+?>
