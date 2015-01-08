63955d4ad96e90f2b3fdbfbf7c9d5d42aea1ef82
thaihonglan@gmail.com <thaihonglan@gmail.com@2c5846bf-b5bc-a74a-191c-b3306825488e>
2015-01-08 21:15:33 +0000

    Daskboard : Google Analytics data & charts

---
+++ trunk/modules/admin/models/Dashboard.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\modules\admin\models;
+
+use Yii;
+use yii\base\Model;
+use app\models\Admin;
+
+
+class Dashboard extends Model
+{
+    public function getPageView()
+    {
+        
+        
+        
+        
+        return '';
+    }
+}
