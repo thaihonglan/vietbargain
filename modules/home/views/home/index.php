52316427742354dd3515b4f84b5cc38d27bd2e7e
joephankm <joephankm@gmail.com>
2015-01-20 15:09:56 +0000

    middle commit

--- modules/home/views/home/index.php
+++ modules/home/views/home/index.php
@@ -4,40 +4,33 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 ?>
 
-<div class="container">
-	<div class="row">
-		<div class="col-lg-8 col-md-8">
-		<?php foreach ($dataProvider->getModels() as $post): ?>
-			<!-- POST -->
-			<div class="post">
-				<div class="wrap-ut pull-left">
-					<div class="userinfo pull-left">
-						<div class="avatar">
-							<img src="<?= Yii::$app->params['postImagePath']['icon'] . (($post->user->avatar) ? $post->user->avatar : Yii::$app->params['postNoImage']) ?>" alt="" />
-							<div class="status green">&nbsp;</div>
-						</div>
+<?php foreach ($dataProvider->getModels() as $post): ?>
+<!-- POST -->
+<div class="post">
+	<div class="wrap-ut pull-left">
+		<div class="userinfo pull-left">
+			<div class="avatar">
+				<img src="<?= Yii::$app->params['userImagePath']['icon'] . (($post->user->avatar) ? $post->user->avatar : Yii::$app->params['userNoImage']) ?>" alt="" height="37" width="37" />
+				<div class="status green">&nbsp;</div>
+			</div>
+		</div>
+		<div class="posttext pull-left">
+			<h2><?= Html::a($post->title, ['topic/view', 'p' => $post->id]) ?></h2>
+			<p><?= $post->short_content ?></p>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="postinfo pull-left">
+		<div class="comments">
+			<div class="commentbg">
+				<?= $post->comment_number ?>
+				<div class="mark"></div>
+			</div>
 
-						<div class="icons">
-<!-- 							<img src="/home/images/icon1.jpg" alt="" /><img src="/home/images/icon4.jpg" alt="" /> -->
-						</div>
-					</div>
-					<div class="posttext pull-left">
-						<h2><?= Html::a($post->title, ['topic/view', 'p' => $post->id]) ?></h2>
-						<p>It's one thing to subject yourself to a Halloween costume mishap because, hey, that's your prerogative.</p>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				<div class="postinfo pull-left">
-					<div class="comments">
-						<div class="commentbg">
-							560
-							<div class="mark"></div>
-						</div>
-
-					</div>
-					<div class="views"><i class="fa fa-eye"></i> 1,568</div>
-					<div class="time"><i class="fa fa-clock-o"></i> 24 min</div>
-				</div>
-				<div class="clearfix"></div>
-			</div><!-- POST -->
-		<?php endforeach; ?>
+		</div>
+		<div class="views"><i class="fa fa-eye"></i> <?= $post->view_number ?></div>
+		<div class="time"><i class="fa fa-clock-o"></i> <?= $post->create_datetime ?></div>
+	</div>
+	<div class="clearfix"></div>
+</div><!-- POST -->
+<?php endforeach; ?>
--- modules/home/views/topic/show.php
+++ modules/home/views/topic/show.php
@@ -1,9 +1,46 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>topic/show</h1>
+use yii\helpers\Html;
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+/* @var $this yii\web\View */
+/* @var $dataProvider app\modules\home\models\PostSearch */
+?>
+
+<?php foreach ($dataProvider->getModels() as $model): ?>
+<!-- POST -->
+<div class="post">
+	<div class="topwrap">
+		<div class="userinfo pull-left">
+			<div class="avatar">
+				<img src="<?= Yii::$app->params['userImagePath']['icon'] . (($model->user->avatar) ? $model->user->avatar : Yii::$app->params['userNoImage']) ?>" alt="" height="37" width="37" />
+				<div class="status red">&nbsp;</div>
+			</div>
+		</div>
+		<div class="posttext pull-left">
+			<h2><?= Html::a($model->title, ['topic/view', 'p' => $model->id]) ?></h2>
+			<p><?= $model->short_content ?></p>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+
+	<div class="postinfobot">
+		<div class="likeblock pull-left">
+			<a href="#" class="up"><i class="fa fa-thumbs-o-up"></i><?= $model->like_number ?></a>
+			<a href="#" class="down"><i class="fa fa-thumbs-o-down"></i><?= $model->dislike_number ?></a>
+		</div>
+
+		<div class="prev pull-left">
+			<a href="#"><i class="fa fa-reply"></i></a>
+		</div>
+
+		<div class="posted pull-left"><i class="fa fa-clock-o"></i> Posted on : <?= Yii::$app->formatter->asDatetime($model->create_datetime, 'long') ?></div>
+
+		<div class="next pull-right">
+<!-- 			<a href="#"><i class="fa fa-share"></i></a> -->
+
+<!-- 			<a href="#"><i class="fa fa-flag"></i></a> -->
+		</div>
+
+		<div class="clearfix"></div>
+	</div>
+</div><!-- POST -->
+<?php endforeach; ?>
